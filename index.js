84f73126a423aea88e9dbec381345d35cd9caf0a
Jacilyn Hayden <jacihayden@gmail.com>
2018-08-25 23:01:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function scuberGreetingForFeet(distance) {
   let result
-  if (distance <= 400) {
+  if (distance <= 400 || distance ) {
     result = 'This one is on me!';
   }
   else if (distance > 2000) {
@@ -12,28 +12,4 @@ function scuberGreetingForFeet(distance) {
   else {
     result = 'Five bucks'
   }
-}
-
-//   describe(''ternaryCheckCity(), function () {
-//     it('returns "Ok, sounds good." when the city is NYC', function () {
-//       expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
-
-//     it('should return "No go." if the destination city is not NYC', function () {
-//       expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
-function ternaryCheckCity() {
-  
-}
-
-//   describe('switchOnCharmFromTip()', function () {
-//     it('should return "Thank you so much." if the tip is generous', function () {
-//       expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
-
-//     it('should return "Thank you." if the tip is not as generous', function () {
-//       expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
-
-//     it('should return "Bye." if anything else', function () {
-//       expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
-
-function switchOnCharmFromTip() {
-  
 }
