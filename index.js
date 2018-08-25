08d820629001949e5e1b066818988d10e115cb74
Jacilyn Hayden <jacihayden@gmail.com>
2018-08-25 22:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,13 @@
 
 scuberGreetingForFeet()
 
-//   describe('ternaryCheckCity()', function () {
+//   describe(''ternaryCheckCity(), function () {
 //     it('returns "Ok, sounds good." when the city is NYC', function () {
 //       expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
 
 //     it('should return "No go." if the destination city is not NYC', function () {
 //       expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+ternaryCheckCity()
 
 //   describe('switchOnCharmFromTip()', function () {
 //     it('should return "Thank you so much." if the tip is generous', function () {
@@ -27,3 +28,4 @@ scuberGreetingForFeet()
 //     it('should return "Bye." if anything else', function () {
 //       expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
 
+switchOnCharmFromTip()
