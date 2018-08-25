a32eb8f3fe93f0d684ccfac6f02cc55d5d2f9b2f
Jacilyn Hayden <jacihayden@gmail.com>
2018-08-25 23:04:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ function scuberGreetingForFeet(distance) {
   if (distance <= 400 ) {
     result = 'This one is on me!';
   }
-  else if (distance > 2000) {
-    result = 'I will gladly take your thirty bucks.';
-  }
-  else if (2000 < distance > 2500) {
-    result = 'No can do.'
-  }
-  else {
-    result = ''
-  }
   
   result
 }
